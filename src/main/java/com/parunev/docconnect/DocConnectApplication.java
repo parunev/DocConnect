d2bcea64a15716eaac5773fdb5a2a88db6aaa79b
Martin Parunev <martin.parunev@university.mentormate.com>
2023-09-14 18:19:11 +0300

    init: dependencies, properties

---
+++ src/main/java/com/parunev/docconnect/DocConnectApplication.java
@@ -0,0 +1,13 @@
+package com.parunev.docconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parunev/docconnect/DocConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parunev.docconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
