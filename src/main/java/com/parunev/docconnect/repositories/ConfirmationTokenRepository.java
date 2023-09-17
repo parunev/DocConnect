77251da1ca7cb9d1b414b6627c9513c71523e891
Martin Parunev <martin.parunev@university.mentormate.com>
2023-09-17 13:41:42 +0300

    on class doc for PasswordTokenRepository & ConfirmationTokenRepository

--- src/main/java/com/parunev/docconnect/repositories/ConfirmationTokenRepository.java
+++ src/main/java/com/parunev/docconnect/repositories/ConfirmationTokenRepository.java
@@ -10,10 +10,31 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+/**
+ * The `ConfirmationTokenRepository` interface is a Spring Data JPA repository for the `ConfirmationToken` entity.
+ * It provides methods for performing CRUD (Create, Read, Update, Delete) operations on `ConfirmationToken` objects
+ * in the database.
+ * This repository includes custom query methods to find a `ConfirmationToken` by its associated token string
+ * and to update the confirmation timestamp.
+ *
+ */
 @Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    /**
+     * Retrieves a `ConfirmationToken` entity by its associated token string.
+     *
+     * @param token The token string to search for.
+     * @return An optional `ConfirmationToken` object if found, or an empty optional if not found.
+     */
     Optional<ConfirmationToken> findByToken(String token);
 
+    /**
+     * Updates the confirmation timestamp for a `ConfirmationToken` with the specified token.
+     *
+     * @param token The token string of the confirmation token to update.
+     * @param now   The timestamp to set as the confirmation time.
+     */
     @Transactional
     @Modifying
     @Query("UPDATE CONFIRMATION_TOKENS c SET c.confirmedAt = ?2 WHERE c.token = ?1")
--- src/main/java/com/parunev/docconnect/repositories/PasswordTokenRepository.java
+++ src/main/java/com/parunev/docconnect/repositories/PasswordTokenRepository.java
@@ -6,7 +6,20 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * The `PasswordTokenRepository` interface is a Spring Data JPA repository for the `PasswordToken` entity.
+ * It provides methods for performing CRUD (Create, Read, Update, Delete) operations on `PasswordToken` objects
+ * in the database.
+ * This repository includes a custom query method to find a `PasswordToken` by its associated token.
+ */
 @Repository
 public interface PasswordTokenRepository extends JpaRepository<PasswordToken, Long> {
+
+    /**
+     * Retrieves a `PasswordToken` entity by its associated token string.
+     *
+     * @param token The token string to search for.
+     * @return An optional `PasswordToken` object if found, or an empty optional if not found.
+     */
     Optional<PasswordToken> findByToken(String token);
 }
